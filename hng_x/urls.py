7af25c829997f3897a0a46ff05643c129962c3bd
Gerald__TM <taiyemusa1582@gmail.com>
2023-09-12 21:34:05 +0100

    added ReDoc url for documentation

--- hng_x/urls.py
+++ hng_x/urls.py
@@ -37,4 +37,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('restapi.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui( # new
+'redoc', cache_timeout=0), name='schema-redoc'),
 ]
